cd919882b517546f32fe573fe5aaabea2856bc51
hanswood <hanswood@icloud.com>
2026-02-23 20:40:07 +0800

    fix: 允许所有网站跨域访问。

--- src/main/java/com/example/movie/config/CorsConfig.java
+++ src/main/java/com/example/movie/config/CorsConfig.java
@@ -1,19 +1,24 @@
 package com.example.movie.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 允许跨域的路径
-                .allowedOrigins("http://localhost:5173")  // 允许跨域的源
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 允许的请求方法
-                .allowedHeaders("*")  // 允许的请求头
-                .allowCredentials(true)  // 允许携带Cookie
-                .maxAge(3600);  // 预检请求的有效期
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 允许所有源
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
